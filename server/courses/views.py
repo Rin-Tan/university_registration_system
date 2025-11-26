71201e5344fcfc1d97d463f9c55146c8b552980b
Azin Kameli <Azin.Kameli904@Gmail.com>
2025-11-27 01:06:42 +0330

    chore: add api app & course model fields

--- server/courses/views.py
+++ server/courses/views.py
@@ -1,4 +1,7 @@
 from django.shortcuts import render
+from .models import Course
 
 def index(request):
-    return render(request, 'courses\index.html')
+    return render(request, 'courses\index.html', {
+        'Courses': Course.objects.all()
+    })
