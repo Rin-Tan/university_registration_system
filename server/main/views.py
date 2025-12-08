72d51144cdce5cd544a2e5a7275bc1bee10dc86b
sadeghkarami <sadeghkarami2001@gmail.com>
2025-12-08 23:55:22 +0330

    Fixed 404 error on root path in old branch to allow rendering login.html

--- server/main/views.py
+++ server/main/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
-
+from django.views import View
 def dashboard(request):
     return render(request, 'dashboard.html')
-
+class LoginRenderView(View):
+    def get(self, request):
+        return render(request, 'login.html')
